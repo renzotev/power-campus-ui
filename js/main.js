b746d611a5805df5bc1cff774567441a481a5f9f
Renzo Telenta <renzotelenta@gmail.com>
2014-05-22 17:00:36 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* contador numeros formulario */
 $("#UPTraDocInformacionGeneral1_txtPerfilProf").keyup(function() {
     var len = this.value.length;
     if (len >= 1501) {
@@ -7,6 +8,8 @@ $("#UPTraDocInformacionGeneral1_txtPerfilProf").keyup(function() {
     }
 });
 
+
+/* circulo sidebar */
 var progress1 = new Charts.CircleProgress(
 'chart1', 
 '', 
@@ -20,3 +23,10 @@ circle_percent,
 });
 
 progress1.draw()
+
+
+/* ver mas */
+
+$(".informacion-wrapper.consult .ver-mas").on("click", function () {
+  $(this).siblings( ".ver-mas-detalle" ).toggle("fast");
+});
